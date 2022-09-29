f493ad7f43d62fc41b145938b95fa9ad62894afd
Camila Pessoa <caape13@hotmail.com>
2022-09-29 10:29:56 +0100

    add expoting components index.js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+
+ReactDOM.render(
+  <App />, //that's my component
+  document.getElementById('root')
+  )
+  
+
+
+
+  
+  // const App = () => {
+  //   return(
+  //     <h1>Hello World</h1>
+  //   )
+  // }
+  
+  // class App extends React.Component {
+  //   render() {
+  //     return <h1>Hello World</h1>
+  //   }
+  // }
+// Render => display sth in web browser
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// // import App from './App';
+
+// // ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(
+//   <h1>Hello World</h1>,
+//   document.getElementById('root')
+// )
