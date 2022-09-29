c53954fccc2e4ebb403b96fbc973d6de9522bcf0
Camila Pessoa <caape13@hotmail.com>
2022-09-29 10:32:28 +0100

    add expoting components index.js

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Album } from './Album';
+export { default as Artist} from './Artist';
+export { default as Form } from './Form';
+export {default as Liked} from './Liked';
+
+
+
+// exportar components files aqui
